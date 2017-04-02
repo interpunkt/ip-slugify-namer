b72bab17da92f14c8100c67b5702c98acad7cab3
Daniel Lehrner <daniel@lehrner.me>
2017-04-02 07:52:48 +0200

    [mod] check if file already exists, if yes add a counter to the end of the file [mod] limit counter to max 100, fall back to Vich Uploader unique id if not successful

--- src/SlugifyNamer.php
+++ src/SlugifyNamer.php
@@ -5,6 +5,7 @@ namespace Ip\SlugifyNamer;
 use Cocur\Slugify\Slugify;
 use Vich\UploaderBundle\Mapping\PropertyMapping;
 use Vich\UploaderBundle\Naming\NamerInterface;
+use Vich\UploaderBundle\Naming\UniqidNamer;
 
 class SlugifyNamer implements NamerInterface
 {
@@ -20,6 +21,7 @@ class SlugifyNamer implements NamerInterface
     {
         $file = $mapping->getFile($object);
         $fileExtension = $file->getClientOriginalExtension();
+        $uploadDestination = $mapping->getUploadDestination();
 
         $fileNameWithoutExtension = basename(
             $file->getClientOriginalName(),
@@ -27,8 +29,26 @@ class SlugifyNamer implements NamerInterface
         );
 
         $slugify = new Slugify();
-        $slugifiedFileName = $slugify->slugify($fileNameWithoutExtension) . '.' . $fileExtension;
+        $slugifiedFileNameWithoutExtension = $slugify->slugify($fileNameWithoutExtension);
+        $slugifiedFileName = $slugifiedFileNameWithoutExtension . '.' . $fileExtension;
 
-        return $slugifiedFileName;
+        // return slugified file name, if the file doesn't exist yet
+        if (!file_exists($uploadDestination . DIRECTORY_SEPARATOR . $slugifiedFileName)) {
+            return $slugifiedFileName;
+        }
+
+        // try to add a counter up to 100 to the filename and check if it already exists
+        for ($i = 1; $i <= 100; ++$i) {
+            $fileNameWithCounter = $slugifiedFileNameWithoutExtension . "_$i." . $fileExtension;
+
+            if (!file_exists($fileNameWithCounter)) {
+                return $fileNameWithCounter;
+            }
+        }
+
+        // fallback to Vich Uploader unique id namer
+        $vichUniqidNamer = new UniqidNamer();
+
+        return $vichUniqidNamer->name($object, $mapping);
     }
 }
